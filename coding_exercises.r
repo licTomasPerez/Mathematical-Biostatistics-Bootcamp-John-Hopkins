9ddc4fa07c3f87527a3ec644fb83d9257d355b46
Tomás Pérez <54460449+licTomasPerez@users.noreply.github.com>
2022-04-07 23:14:24 -0300

    Create coding_exercises.r

---
+++ coding_exercises.r
@@ -0,0 +1,11 @@
+# Quiz 1 - Ex. 3
+P_AvB <- 0.17
+P_B <- 0.12
+P_AyB <- 0.06
+
+P_A <- P_AvB - P_B + P_AyB
+P_A
+
+# Quiz 1 - Ex. 3
+# This density is a box of inferior lenght 1. The point so that the area below it is  
+qunif(0.75)
