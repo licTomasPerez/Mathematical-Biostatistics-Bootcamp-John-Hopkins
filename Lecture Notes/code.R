138208d3aede66281437b914292d96128bde331a
Tomás Pérez <54460449+licTomasPerez@users.noreply.github.com>
2022-05-12 19:41:50 -0300

    Update code.R

--- Lecture Notes/code.R
+++ Lecture Notes/code.R
@@ -31,3 +31,22 @@ polygon(c(x, rev(x)), c(dnorm(x), rep(0,length(x))), col="salmon")
 text(mean(x), mean(dnorm(x))+0.2, "2.5%", cex=2)
 text(qnorm(.025), .01, "1.96",cex=2)
 text(0, dnorm(0)/5, "95%",cex=2)
+
+
+-------------- 
+#Student's two sided test 
+
+xval <-  seq(-4, 4, length=1000)
+yval <- dt(xval, 15)
+plot(xval, yval, type = "l", axes=TRUE, frame=FALSE, lwd = 3, xlab="", ylab= "")
+x <- seq(qt(.975, 15), 4, length=100)
+polygon(c(x, rev(x)), c(dt(x,15), rep(0,length(x))), col="salmon")
+text(mean(x), mean(dt(xval, 16-1))+0.2, "2.5%", cex=2)
+text(qt(.975,15), .01, "2.13",cex=2)
+x <- seq(-3.2, qt(.025,16), length=100)
+polygon(c(x, rev(x)), c(dt(x,15), rep(0,length(x))), col="salmon")
+text(mean(x), mean(dt(xval, 16-1))+0.2, "2.5%", cex=2)
+text(qt(.025,15), .01, "2.13",cex=2)
+text(0, dt(0,15)/5, "95%", cex=2)
+
+
